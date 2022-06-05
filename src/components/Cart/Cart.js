54542c5ed951a2469dbd408a5be9f9ceadfecb66
cabrit0 <cabrit0o.dev@gmail.com>
2022-06-05 20:53:37 +0100

    commit main

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,13 @@
+import classes from "./Cart.module.css";
+
+const Cart = (props) => {
+  const cartItems = [{ id: "c1", name: "sushi", amount: 2, price: 12.99 }];
+  return (
+    <div>
+      cartItem
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+export default Cart;
